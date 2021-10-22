acd56c64830ebfdd415b645ac198417ebf632864
charuvijayan <charuvijay6@gmail.com>
2021-10-22 08:44:43 +0530

    my task

---
+++ src/A.java
@@ -0,0 +1,44 @@
+
+public class A {
+
+public int  test(int a,int b) {
+	int c=a+b;
+	return c;
+	
+	
+
+}	
+		
+	}
+
+
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/B.java
@@ -0,0 +1,16 @@
+
+public class B {
+
+	public static void main(String[] args) {
+	
+		A a = new A();
+		int r = a.test(40, 10);
+		//System.out.println(r);
+		if (r==50) {
+			System.out.println("valid");
+		}else {
+			System.out.println("invalid");
+		}
+	}}
+
+
